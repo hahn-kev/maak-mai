df393b245019d5c8a5ca857fa8a3ee35ab36694c
Kevin Hahn <kevin_hahn@sil.org>
2025-08-16 11:51:26 +0700

    add some files which didn't get committed

---
+++ app/src/test/java/org/hahn/maakmai/util/OpenGraphUtilsTest.kt
@@ -0,0 +1,61 @@
+package org.hahn.maakmai.util
+
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class OpenGraphUtilsTest {
+
+    @Test
+    fun `test HTML entity decoding in meta tags`() {
+        // Create HTML with encoded entities
+        val html = """
+            <html>
+            <head>
+                <meta property="og:title" content="Test &quot;quoted&quot; title" />
+                <meta property="og:description" content="Description with &amp; and &lt;tags&gt;" />
+                <title>Regular &quot;title&quot; with entities</title>
+            </head>
+            <body>
+                <p>Some content</p>
+            </body>
+            </html>
+        """.trimIndent()
+
+        // Use reflection to access private methods for testing
+        val parseMethod = OpenGraphUtils::class.java.getDeclaredMethod("parseOpenGraphMetadata", String::class.java)
+        parseMethod.isAccessible = true
+
+        // Parse the HTML
+        val metadata = parseMethod.invoke(OpenGraphUtils, html) as OpenGraphUtils.OpenGraphMetadata
+
+        // Verify the entities are decoded
+        assertEquals("Test \"quoted\" title", metadata.title)
+        assertEquals("Description with & and <tags>", metadata.description)
+    }
+
+    @Test
+    fun `test HTML entity decoding in title tag`() {
+        // Create HTML with encoded entities in title but no OG tags
+        val html = """
+            <html>
+            <head>
+                <title>Regular &quot;title&quot; with &amp; entities</title>
+            </head>
+            <body>
+                <p>Some content</p>
+            </body>
+            </html>
+        """.trimIndent()
+
+        // Use reflection to access private methods for testing
+        val parseMethod = OpenGraphUtils::class.java.getDeclaredMethod("parseOpenGraphMetadata", String::class.java)
+        parseMethod.isAccessible = true
+
+        // Parse the HTML
+        val metadata = parseMethod.invoke(OpenGraphUtils, html) as OpenGraphUtils.OpenGraphMetadata
+
+        // Verify the entities in the title are decoded
+        assertEquals("Regular \"title\" with & entities", metadata.title)
+    }
+}
